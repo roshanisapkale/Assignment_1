ae33f58582f3352a07d3b99c01b111b0299f2136
Roshani Sapkale <>
2023-02-11 10:09:28 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+ /* Q.10] Write program using Switch-case statement for days of months.  */
+
+  let day = prompt("\nEnter a no for day of month:");
+  day = Number.parseInt(day);
+  switch (day) {
+    case 1:
+      console.log("\n It's monday.");
+      break;
+    case 2:
+      console.log("\n It's tuesday.");
+      break;
+    case 3:
+      console.log("\n It's wednesday.");
+      break;
+    case 4:
+      console.log("\n It's thursday.");
+      break;
+    case 5:
+      console.log("\n It's friday.");
+      break;
+    case 6:
+      console.log("\n It's saturday");
+      break;
+    case 7:
+      console.log("\n It's sunday.");
+      break;
+}
